fe79f61addc826adc98c8a3252d979660804fc5c
Mike Veneziale <falciformdesign@gmail.com>
2021-09-07 14:42:20 -0500

    added files for creating and seeding database table

---
+++ db/db.sql
@@ -0,0 +1,5 @@
+
+DROP DATABASE IF EXISTS manager;
+
+CREATE DATABASE employees;
+USE employees;
---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES
+  ('Shane', 'Cross', 2, 2),
+  ('Rob', 'Dyrdek', 1, 0),
+  ('Kelly', 'Hart', 2, 1),
+  ('Bob', 'Burnquist', 2, 1),
+  ('Kareem', 'Campbell', 2, 1);
