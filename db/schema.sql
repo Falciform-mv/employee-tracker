d6b3d7a0c18300035569bb5f5f1f34b591140e28
Mike Veneziale <falciformdesign@gmail.com>
2021-09-11 11:01:40 -0500

    created 2nd table for roles

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,12 @@
+
+CREATE TABLE roles (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary INTEGER,
+  department_id INTEGER
+);
+
+
 CREATE TABLE employees (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,11 @@
 
+INSERT INTO roles (title, salary, department_id)
+VALUES
+  ('Manager', 100000, 10),
+  ('Engineer', 70000, 11),
+  ('Marketer', 60000, 12);
+
+
 INSERT INTO employees (first_name, last_name, role_id, manager_id)
 VALUES
   ('Shane', 'Cross', 2, 2),
