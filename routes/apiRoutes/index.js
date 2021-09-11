b0ada53e887cdd3c32d260c957fa1a49e3a367f4
Mike Veneziale <falciformdesign@gmail.com>
2021-09-11 14:47:31 -0500

    updated index

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -4,5 +4,8 @@ const router = express.Router();
 
 router.use(require('./employeeRoutes'));
 router.use(require('./roleRoutes'));
+router.use(require('./departmentRoutes'))
+
+
 
 module.exports = router;
