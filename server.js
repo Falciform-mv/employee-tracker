ae93792ac75d3b97d48e2f9ff02573b6a5af1928
Mike Veneziale <falciformdesign@gmail.com>
2021-09-10 20:44:21 -0500

    added CRUD

--- server.js
+++ server.js
@@ -21,10 +21,40 @@ const db = mysql.createConnection(
 );
 
 
+// displays all employees from database table
+// db.query(`SELECT * FROM employees`, (err, rows) => {
+//   console.log(rows);
+// });
 
-db.query(`SELECT * FROM employees`, (err, rows) => {
-  console.log(rows);
+// gets a single employee
+// db.query(`SELECT * FROM employees WHERE id = 1`, (err, row) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(row);
+// });
+
+// deletes an employee
+// db.query(`DELETE FROM employees WHERE id = ?`, 1, (err, result) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(result);
+// });
+
+// creates an employee
+const sql = `INSERT INTO employees (id, first_name, last_name, role_id, manager_id)
+VALUES (?, ?, ?, ?, ?)`;
+const params = [1, 'Shane', 'Cross', 2, 2];
+
+db.query(sql, params, (err, result) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(result);
 });
+
+
 
 // default response for any other request (not found)
 app.use((req, res) => {
