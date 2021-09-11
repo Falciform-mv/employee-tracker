d52019436de36c0ac39fc0f45c0f560bb1aaf05f
Mike Veneziale <falciformdesign@gmail.com>
2021-09-10 21:40:49 -0500

    express routes for CRUD have been added

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 
+const inputCheck = require('./utils/inputCheck');
 
 const mysql = require('mysql2');
 
@@ -22,36 +23,83 @@ const db = mysql.createConnection(
 
 
 // displays all employees from database table
-// db.query(`SELECT * FROM employees`, (err, rows) => {
-//   console.log(rows);
-// });
+app.get ('/api/employees', (req, res) => {
+  const sql = `SELECT * FROM employees`;
+
+  db.query(sql, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: rows
+    });
+  });
+});
 
 // gets a single employee
-// db.query(`SELECT * FROM employees WHERE id = 1`, (err, row) => {
-//   if (err) {
-//     console.log(err);
-//   }
-//   console.log(row);
-// });
+app.get('/api/employee/:id', (req, res) => {
+  const sql = `SELECT * FROM employees WHERE id = ?`;
+  const params = [req.params.id];
+
+  db.query(sql, params, (err, row) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: row
+    });
+  });
+});
 
 // deletes an employee
-// db.query(`DELETE FROM employees WHERE id = ?`, 1, (err, result) => {
-//   if (err) {
-//     console.log(err);
-//   }
-//   console.log(result);
-// });
+app.delete('/api/employee/:id', (req, res) => {
+  const sql = `DELETE FROM employees WHERE id = ?`;
+  const params = [req.params.id];
+
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.statusMessage(400).json({ error: res.message });
+    } else if (!result.affectedRows) {
+      res.json({
+        message: 'Employee not found'
+      });
+    } else {
+      res.json({
+        message: 'deleted',
+        changes: result.affectedRows,
+        id: req.params.id
+      });
+    }
+  });
+});
 
 // creates an employee
-const sql = `INSERT INTO employees (id, first_name, last_name, role_id, manager_id)
-VALUES (?, ?, ?, ?, ?)`;
-const params = [1, 'Shane', 'Cross', 2, 2];
-
-db.query(sql, params, (err, result) => {
-  if (err) {
-    console.log(err);
+app.post('/api/employee', ({ body }, res) => {
+  const errors = inputCheck(body, 'first_name', 'last_name', 'role_id', 'manager_id');
+  if (errors) {
+    res.status(400).json({ error: errors });
+    return;
   }
-  console.log(result);
+
+  const sql = `INSERT INTO employees (first_name, last_name, role_id, manager_id)
+    VALUES (?, ?, ?, ?)`;
+  const params = [body.first_name, body.last_name, body.role_id, body.manager_id];
+
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data:body
+    });
+  });
+
 });
 
 
